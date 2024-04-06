79ee94fe4d5687ece1cc959500209d31e90db804
xpinar <107260019+xpinar@users.noreply.github.com>
2024-04-06 21:01:35 +0200

    Only pellets work will test others

--- Assets/Scripts/PacmanIA.cs
+++ Assets/Scripts/PacmanIA.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 public class PacmanAI : MonoBehaviour
 {
-    public Transform target;
     private Vector2 currentDirection;
     private Vector2 nextDirection;
 
@@ -11,10 +10,14 @@ public class PacmanAI : MonoBehaviour
     private float maxDistanceToGhosts; // Nouvelle variable globale pour stocker la distance maximale aux fantômes
 
     private Pacman pacman;
+    private Pellet pellet;
+    private PowerPellet powerPellet;
 
     private void Awake()
     {
         pacman = GetComponent<Pacman>();
+        pellet = GetComponent<Pellet>();
+        powerPellet = GetComponent<PowerPellet>();
     }
 
     void Start()
@@ -51,26 +54,42 @@ public class PacmanAI : MonoBehaviour
     private Vector2 UpdateDirection()
     {
         float distanceToTargetWeight = 2.0f;
-        float avoidGhostsWeight = 2.0f;
+        float avoidGhostsWeight = 0.0f;
+        float powerPelletWeight = 0.0f;
 
         (Vector2 directionDistanceToTarget, float score1) = GetDirectionWithMinDistanceToTarget();
         (Vector2 directionAvoidGhosts, float score2) = GetDirectionAwayFromGhosts();
+        (Vector2 directionPowerPellet, float score3) = GetDirectionWithMinDistanceToPowerPellet();
         Debug.Log("Direction 1 :" + directionDistanceToTarget + "Score 1 : " + score1);
         Debug.Log("Direction 2 :" + directionAvoidGhosts + "Score 2 : " + score2);
-
+        Debug.Log("Direction 3 :" + directionPowerPellet + "Score 3 : " + score3);
         Vector2 nextDirection = Vector2.zero;
 
         // Combine all heuristics
         float weightedScore1 = score1 * distanceToTargetWeight;
         float weightedScore2 = score2 * avoidGhostsWeight;
+        float weightedScore3 = score3 * powerPelletWeight;
 
         if (weightedScore1 >= weightedScore2)
         {
-            nextDirection = directionDistanceToTarget;
+            if (weightedScore1 >= weightedScore3)
+            {
+                nextDirection = directionDistanceToTarget;
+            }
         }
-        else
+        else if (weightedScore2 >= weightedScore1)
         {
-            nextDirection = directionAvoidGhosts;
+            if (weightedScore2 >= weightedScore3)
+            {
+                nextDirection = directionAvoidGhosts;
+            }
+        }
+        else if (weightedScore3 >= weightedScore1)
+        {
+            if (weightedScore3 >= weightedScore2)
+            {
+                nextDirection = directionPowerPellet;
+            }
         }
         Debug.Log("NextDirection :" + nextDirection);
 
@@ -87,16 +106,52 @@ public class PacmanAI : MonoBehaviour
         foreach (Vector2 availableDirection in availableDirections)
         {
             Vector3 newPosition = transform.position + new Vector3(availableDirection.x, availableDirection.y);
-            float distanceToTarget = Vector3.Distance(target.position, newPosition);
-
-            // Calcul du score basé sur la distance à la cible (plus la distance est courte, plus le score est élevé)
-            float score = 1 - (distanceToTarget / maxDistance); // Score normalisé entre 0 et 1
-
-            // Mettre à jour la meilleure direction et le meilleur score si nécessaire
-            if (score > bestScore)
+            GameObject[] powerPellets = GameObject.FindGameObjectsWithTag("Pellet");
+            foreach (var pellet in powerPellets)
             {
-                bestScore = score;
-                bestDirection = availableDirection;
+
+                float distanceToTarget = Vector3.Distance(pellet.transform.position, newPosition);
+
+                // Calcul du score basé sur la distance à la cible (plus la distance est courte, plus le score est élevé)
+                float score = 1 - (distanceToTarget / maxDistance); // Score normalisé entre 0 et 1
+
+                // Mettre à jour la meilleure direction et le meilleur score si nécessaire
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    bestDirection = availableDirection;
+                }
+            }
+        }
+
+        return (bestDirection, bestScore);
+    }
+
+    private (Vector2, float) GetDirectionWithMinDistanceToPowerPellet()
+    {
+        float maxDistance = Vector3.Distance(Vector3.zero, new Vector3(20, 20, 0)); // Max possible distance
+        //float minDistance = float.MaxValue;
+        Vector2 bestDirection = Vector2.zero;
+        float bestScore = 0;
+
+        foreach (Vector2 availableDirection in availableDirections)
+        {
+            Vector3 newPosition = transform.position + new Vector3(availableDirection.x, availableDirection.y);
+            GameObject[] powerPellets = GameObject.FindGameObjectsWithTag("PowerPellet");
+            foreach (var powerPellet in powerPellets)
+            {
+
+                float distanceToTarget = Vector3.Distance(powerPellet.transform.position, newPosition);
+
+                // Calcul du score basé sur la distance à la cible (plus la distance est courte, plus le score est élevé)
+                float score = 1 - (distanceToTarget / maxDistance); // Score normalisé entre 0 et 1
+
+                // Mettre à jour la meilleure direction et le meilleur score si nécessaire
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    bestDirection = availableDirection;
+                }
             }
         }
 
@@ -121,7 +176,7 @@ public class PacmanAI : MonoBehaviour
             if (score > bestScore)
             {
                 bestScore = score;
-                bestDirection = direction;
+                bestDirection = -direction;
             }
         }
 
